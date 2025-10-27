e924b1f4a00cfa34e2f2002832537b918e6da390
Shahreyar@11 <shahreyar551@gmail.com>
2025-10-27 11:16:08 +0530

    updated models

--- server/models/user.js
+++ server/models/user.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({  // as mongoose.Schema is a class so we
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     } ,
     password: {
         type: String,
